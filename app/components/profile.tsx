8cb210da28e64baeabd388733a28528a7f867447
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2023-12-12 02:57:58 +0530

    Removed extra icons

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -4,23 +4,13 @@ import HTMLIMG from "../../public/tech/html.svg";
 import CSSIMG from "../../public/tech/css.svg";
 import JSIMG from "../../public/tech/javascript.svg";
 import ReactImg from "../../public/tech/react.svg";
-import NextImg from "../../public/tech/nextjs.svg";
 import NodeImg from "../../public/tech/nodejs.svg";
-import ExpressImg from "../../public/tech/express.svg";
 import MongoDBImg from "../../public/tech/mongodb.svg";
 import AWSImg from "../../public/tech/aws.svg";
 import CPlusPlusImg from "../../public/tech/cPlusPlus.svg";
 import PythonImg from "../../public/tech/python.svg";
-import GitHubImg from "../../public/tech/github.svg";
-import BitBucketImge from "../../public/tech/bitbucket.svg";
-import BootstrapImg from "../../public/tech/bootstrap.svg";
-import FigmaImg from "../../public/tech/figma.svg";
-import TailwindImg from "../../public/tech/tailwind.svg";
-import MaterialImg from "../../public/tech/material.svg";
 import MySQLImg from "../../public/tech/mysql.svg";
-import FirebaseImg from "../../public/tech/firebase.svg";
-import GoDaddy from "../../public/tech/godaddy.svg";
-import PostMan from "../../public/tech/postman.svg";
+import DjangoImg from "../../public/tech/django.svg";
 
 export default function Profile() {
   return (
@@ -47,27 +37,17 @@ export default function Profile() {
         </div>
       </div>
       <div className="bg-gradient-to-b from-white to-gray-100 rounded-b border-2 border-t-0 p-4 flex items-center md:justify-center gap-4 overflow-x-scroll scroll-smooth no-scrollbar">
-        <Image src={HTMLIMG} alt="HTML" height={35} width={35} />
-        <Image src={CSSIMG} alt="CSS" height={35} width={35} />
-        <Image src={JSIMG} alt="JavaScript" height={35} width={35} />
-        <Image src={ReactImg} alt="React Js" height={35} width={35} />
-        <Image src={NextImg} alt="Next Js" height={35} width={35} />
-        <Image src={NodeImg} alt="Node Js" height={35} width={35} />
-        <Image src={ExpressImg} alt="Express Js" height={35} width={35} />
-        <Image src={MongoDBImg} alt="MongoDB" height={35} width={35} />
-        <Image src={AWSImg} alt="AWS" height={35} width={35} />
-        <Image src={CPlusPlusImg} alt="C++" height={35} width={35} />
-        <Image src={PythonImg} alt="Python" height={35} width={35} />
-        <Image src={GitHubImg} alt="GitHub" height={35} width={35} />
-        <Image src={BitBucketImge} alt="BitBucket" height={35} width={35} />
-        <Image src={BootstrapImg} alt="Bootstrap" height={35} width={35} />
-        <Image src={FigmaImg} alt="Figma" height={35} width={35} />
-        <Image src={TailwindImg} alt="Tailwind" height={35} width={35} />
-        <Image src={MaterialImg} alt="Material" height={35} width={35} />
-        <Image src={MySQLImg} alt="MySQL" height={35} width={35} />
-        <Image src={FirebaseImg} alt="Firebase" height={35} width={35} />
-        <Image src={GoDaddy} alt="GoDaddy" height={35} width={35} />
-        <Image src={PostMan} alt="PostMan" height={35} width={35} />
+        <Image src={HTMLIMG} alt="HTML" height={40} width={40} />
+        <Image src={CSSIMG} alt="CSS" height={40} width={40} />
+        <Image src={JSIMG} alt="JavaScript" height={40} width={40} />
+        <Image src={ReactImg} alt="React Js" height={40} width={40} />
+        <Image src={NodeImg} alt="Node Js" height={40} width={40} />
+        <Image src={MongoDBImg} alt="MongoDB" height={40} width={40} />
+        <Image src={MySQLImg} alt="MySQL" height={40} width={40} />
+        <Image src={CPlusPlusImg} alt="C++" height={40} width={40} />
+        <Image src={PythonImg} alt="Python" height={40} width={40} />
+        <Image src={DjangoImg} alt="Django" height={40} width={40} />
+        <Image src={AWSImg} alt="AWS" height={40} width={40} />
       </div>
     </div>
   );
