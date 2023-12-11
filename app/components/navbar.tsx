b25db57abdb77d10dc82279ae254190e03df8a3d
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2023-12-11 16:30:16 +0530

    Completed projects page

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import {
   HomeIcon,
   NewspaperIcon,
@@ -13,21 +13,39 @@ function Navbar() {
 
   return (
     <div className="bg-gradient-to-b from-white to-gray-100 flex justify-center gap-4 my-4 p-2 border shadow rounded text-gray-700 font-semibold">
-      <Link href="/" className={`link ${pathname === '/' ? 'bg-gray-300' : ''} px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center`}>
+      <Link
+        href="/"
+        className={`link ${
+          pathname === "/" ? "bg-gray-300" : ""
+        } px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center`}
+      >
         <HomeIcon className="w-5 h-5" />
         <div className="hidden md:block">Home</div>
       </Link>
-      <Link href="/projects" className={`link ${pathname === '/projects' ? 'bg-gray-300' : ''} px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center`}>
+      <Link
+        href="/projects"
+        className={`link ${
+          pathname === "/projects" ? "bg-gray-300" : ""
+        } px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center`}
+      >
         <CodeBracketSquareIcon className="w-5 h-5" />
         <div className="hidden md:block">Projects</div>
       </Link>
-      <Link href="/Saurabh_Tiwari_Resume.pdf" className="px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center">
-        <NewspaperIcon className="w-5 h-5" />
-        <div className="hidden md:block">Resume</div>
-      </Link>
-      <Link href="/contact" className={`link ${pathname === '/contact' ? 'bg-gray-300' : ''} px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center`}>
+      <Link
+        href="/contact"
+        className={`link ${
+          pathname === "/contact" ? "bg-gray-300" : ""
+        } px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center`}
+      >
         <PhoneIcon className="w-5 h-5" />
         <div className="hidden md:block">Contact</div>
+      </Link>
+      <Link
+        href="/Saurabh_Tiwari_Resume.pdf"
+        className="px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center"
+      >
+        <NewspaperIcon className="w-5 h-5" />
+        <div className="hidden md:block">Resume</div>
       </Link>
     </div>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,14 +1,28 @@
-
-
 function Projects() {
   return (
     <>
       <title>Projects</title>
-      <main>
-        Projects
+      <main className="bg-gradient-to-b from-white to-gray-100 rounded shadow border p-4 mb-4">
+        <div className="text-3xl font-black text-gray-600 text-center mb-4">
+          Projects
+        </div>
+        <div className="grid grid-cols-2 gap-4 text-gray-500">
+          <div className="col-span-1 rounded shadow p-4 bg-gradient-to-b from-white to-gray-100">
+            Project 1 coming soon...
+          </div>
+          <div className="col-span-1 rounded shadow p-4 bg-gradient-to-b from-white to-gray-100">
+            Project 2 coming soon...
+          </div>
+          <div className="col-span-1 rounded shadow p-4 bg-gradient-to-b from-white to-gray-100">
+            Project 3 coming soon...
+          </div>
+          <div className="col-span-1 rounded shadow p-4 bg-gradient-to-b from-white to-gray-100">
+            Project 4 coming soon...
+          </div>
+        </div>
       </main>
     </>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
