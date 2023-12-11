2d1ab8600344e68222abe1e39f6d056aecf2f6d5
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2023-12-11 16:31:37 +0530

    Done formating

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,10 @@ function Footer() {
   return (
     <div className="w-full flex justify-center text-center bg-gradient-to-b from-white to-gray-100 rounded shadow border p-4">
       <div className="font-semibold text-gray-500 flex flex-col gap-2 items-center justify-center">
-        <div>&#169;2023 Designed and developed by <span className="text-gray-700">Saurabh Tiwari</span></div>
+        <div>
+          &#169;2023 Designed and developed by{" "}
+          <span className="text-gray-700">Saurabh Tiwari</span>
+        </div>
         <div className="text-sm">
           <Link
             href="https://github.com/SaurabhTiwari4093/portfolio"
