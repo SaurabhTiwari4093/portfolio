68afd85c7d119f5ca8f110e5a630e4a332cc699d
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2023-12-12 17:26:53 +0530

    Optimized nav bar

---
+++ app/components/navButtons.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+function NavButtons(props: any) {
+  const pathname = usePathname();
+  return (
+    <Link
+      href={props.href}
+      target={props.label === "Resume" ? "_blank" : ""}
+      rel={props.label === "Resume" ? "noopener noreferrer" : ""}
+      className={`link ${
+        pathname === props.href ? "bg-gray-300" : ""
+      } px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center`}
+    >
+      {props.icon}
+      <div className="hidden md:block">{props.label}</div>
+    </Link>
+  );
+}
+
+export default NavButtons;
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,54 +1,34 @@
-"use client";
 import {
   HomeIcon,
   NewspaperIcon,
   CodeBracketSquareIcon,
   PhoneIcon,
 } from "@heroicons/react/24/solid";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import NavButtons from "./navButtons";
 
 function Navbar() {
-  const pathname = usePathname();
-
   return (
     <div className="bg-gradient-to-b from-white to-gray-100 flex justify-center gap-4 my-4 p-2 border shadow rounded text-gray-700 font-semibold">
-      <Link
+      <NavButtons
         href="/"
-        className={`link ${
-          pathname === "/" ? "bg-gray-300" : ""
-        } px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center`}
-      >
-        <HomeIcon className="w-5 h-5" />
-        <div className="hidden md:block">Home</div>
-      </Link>
-      <Link
+        icon={<HomeIcon className="w-5 h-5" />}
+        label="Home"
+      />
+      <NavButtons
         href="/projects"
-        className={`link ${
-          pathname === "/projects" ? "bg-gray-300" : ""
-        } px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center`}
-      >
-        <CodeBracketSquareIcon className="w-5 h-5" />
-        <div className="hidden md:block">Projects</div>
-      </Link>
-      <Link
+        icon={<CodeBracketSquareIcon className="w-5 h-5" />}
+        label="Projects"
+      />
+      <NavButtons
         href="/contact"
-        className={`link ${
-          pathname === "/contact" ? "bg-gray-300" : ""
-        } px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center`}
-      >
-        <PhoneIcon className="w-5 h-5" />
-        <div className="hidden md:block">Contact</div>
-      </Link>
-      <Link
+        icon={<PhoneIcon className="w-5 h-5" />}
+        label="Contact"
+      />
+      <NavButtons
         href="/Saurabh_Tiwari_Resume.pdf"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="px-6 md:px-8 py-2 rounded-full shadow bg-gray-100 hover:bg-gray-300 cursor-pointer flex gap-1 justify-center items-center"
-      >
-        <NewspaperIcon className="w-5 h-5" />
-        <div className="hidden md:block">Resume</div>
-      </Link>
+        icon={<NewspaperIcon className="w-5 h-5" />}
+        label="Resume"
+      />
     </div>
   );
 }
